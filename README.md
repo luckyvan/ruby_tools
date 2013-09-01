3060c71e78ba25795c299e6164905a17a2993b60
luckyvan <luckyvan@luckyvan-VirtualBox.(none)>
2013-09-01 22:17:11 +0800

    readme

---
+++ README.md
@@ -0,0 +1,8 @@
+# ruby_tools
+
+## usage
+* `ng` creates a bash script which can new a game based on a reference game.
+* `merge`creates a bash script which merge all resources files from `from` dir to `to` dir
+
+## how to install
+./install.rb
---
+++ install.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env ruby
+# coding: UTF-8
+$LOAD_PATH.unshift File.dirname(__FILE__) + '/lib'
+
+require 'fileutils'
+
+file_dir = File.expand_path(File.dirname(__FILE__))
+bin_dir =  file_dir + '/bin/'
+
+# add bin to env
+File.open(File.expand_path("~/.bashrc"), "a"){ |f| f.puts "PATH=#{bin_dir}:\$PATH" }
+
+# back up all bin/* files
+back_dir = file_dir + "/back/"
+FileUtils.rm_r back_dir
+FileUtils.mkdir_p(back_dir) unless Dir.exist?(back_dir)
+FileUtils.cp_r bin_dir, back_dir
+
+# create soft links supportted now.
+app_dir = file_dir + '/app/'
+softlinks = {
+  "create_file.rb" => "ct_f",
+  "merge.rb" => "merge",
+  "new_game.rb" => "ng"
+}
+
+Dir.glob(app_dir + '/*').each do |f|
+  file_name = File.basename f
+  puts file_name
+  FileUtils.ln_s(f, bin_dir + '/' + softlinks[file_name], :force => true) if softlinks[file_name]
+end
