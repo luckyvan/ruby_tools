3f4322f0a00fe4841fade8a5860858d57d969fb6
luckyvan <luckyvan@luckyvan-VirtualBox.(none)>
2013-09-25 13:06:44 +0800

    add relative path support in ru

--- app/required_updates.rb
+++ app/required_updates.rb
@@ -7,6 +7,7 @@ require 'fileutils'
 
 #input
 source_path = Pathname.new(ARGV[0])
+source_path = File.absolute_path(source_path)
 
 fail "Invalid path: #{source_path}" unless File.exist?(source_path)
 
