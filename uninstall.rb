aedd6d03d7d543900579389d106db6523d5552da
luckyvan <luckyvan@luckyvan-VirtualBox.(none)>
2013-09-25 11:33:24 +0800

    delete redundant files, add ru tool, update uninstall

--- uninstall.rb
+++ uninstall.rb
@@ -7,15 +7,19 @@ require 'fileutils'
 
 file_dir = File.expand_path(File.dirname(__FILE__))
 bin_dir =  file_dir + '/bin/'
+back_dir = file_dir + '/back/'
 
 softlinks = {
   "create_file.rb" => "ct_f",
   "merge.rb" => "merge",
   "new_game.rb" => "ng",
   "symbols.rb" => "symbols",
+  "required_updates.rb" => "ru",
 }
 
 
 softlinks.each_value do |bin|
   FileUtils.rm_f(bin_dir + '/' + bin)
 end
+
+FileUtils.rm_rf back_dir
